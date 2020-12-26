5b71df326cd9b6f3e45997e8e1d790b9a35ed6ab
Mapleia <kaede.ito2001@gmail.com>
2020-12-26 06:30:01 -0500

    Moved file processing to actual file processing class.

--- src/main/model/LogCompare.java
+++ src/main/model/LogCompare.java
@@ -4,8 +4,14 @@ import org.apache.commons.io.FilenameUtils;
 import org.json.JSONObject;
 import persistence.JsonReader;
 
+import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
 
 // class that does all of the comparisons and gives an output
 public class LogCompare {
@@ -25,5 +31,63 @@ public class LogCompare {
         logger.upload();
 
         return primary.toJson(logger.uptimePercentile(), logger.dpsPercentiles());
+    }
+
+    public static List<File> processFiles(Object[] toDo, JTextArea log) {
+
+        FileManager finder = new FileManager(toDo);
+        // Sort files into either needing a Gw2EI parse or not.
+        Map<String, List<File>> sorted = finder.sortShouldEIParse();
+
+        // Parse all the files that need a json file.
+        invokeEliteInsight(sorted.get("toEI"), log);
+
+        // Add all the new json files
+        List<File> files = sorted.get("json");
+        FileManager findJson = new FileManager(sorted.get("toEI").toArray());
+        files.addAll(findJson.findEIParsedFiles());
+
+        return files;
+    }
+
+    // EFFECT: create a string array of commands for the EliteInsight parser
+    private static String[] createCommandArray(List<File> evtcFiles) {
+        String[] result = new String[evtcFiles.size() + 3];
+        result[0] = "./data/GW2EI/GuildWars2EliteInsights.exe";
+        result[1] = "-c";
+        try {
+            result[2] = "\"" + new File("./data/assets/app.conf").getCanonicalPath() + "\"";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < evtcFiles.size(); i++) {
+            File file = evtcFiles.get(i);
+            try {
+                result[3 + i] = "\"" + file.getCanonicalPath() + "\"";
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    // EFFECT: run GuildWars2EliteInsights.exe
+    private static void invokeEliteInsight(List<File> evtcFiles, JTextArea log) {
+        try {
+            String[] params = createCommandArray(evtcFiles);
+
+            Process p = Runtime.getRuntime().exec(params);
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line;
+            while ((line = input.readLine()) != null) {
+                log.append(line+ "\n");
+            }
+
+            input.close();
+        } catch (IOException e) {
+            log.append("Exception occurred: " + e+ "\n");
+        }
+
     }
 }
--- src/main/ui/MainPanel.java
+++ src/main/ui/MainPanel.java
@@ -1,6 +1,5 @@
 package ui;
 
-import model.FileManager;
 import model.LogCompare;
 import org.apache.commons.io.FilenameUtils;
 import org.json.JSONObject;
@@ -8,31 +7,27 @@ import persistence.JsonWriter;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
 // the main panel where users interact with the app
 public class MainPanel extends JPanel {
     public static final String DEFAULT_SAVE = new JFileChooser().getFileSystemView().getDefaultDirectory().toString()
             + "/LogCompare/";
-    private final DefaultListModel<File> fileToDo;
+    private final DefaultListModel<File> fileToDo = new DefaultListModel<>();
     private final String pass;
     private Connection con;
     private String saveLocation = DEFAULT_SAVE;
-    private final JTextArea log;
+    private final JTextArea log = new JTextArea(10, 50);
 
     public MainPanel(String pass, Connection con) {
         this.pass = pass;
         this.con = con;
-        fileToDo = new DefaultListModel<>();
-        log = new JTextArea(10, 50);
+
         log.setEditable(false);
 
         add(new FilesPanel(fileToDo));
@@ -92,46 +87,44 @@ public class MainPanel extends JPanel {
     private JButton confirmAndCompare() {
         JButton button = new JButton("Confirm and Compare");
         button.addActionListener(e -> {
-            try {
-                if (con.isClosed()) {
-                    con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/logcompare", "root", pass);
-                }
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
+            // if needed
+            reopenConnection();
+            // if needed
+            createSaveFolder();
 
-            if (saveLocation.equals(DEFAULT_SAVE)) {
-                File theDir = new File(DEFAULT_SAVE);
-                if (!theDir.exists()){
-                    theDir.mkdirs();
-                }
-            }
-
-            FileManager finder = new FileManager(fileToDo.toArray());
-            // Sort files into either needing a Gw2EI parse or not.
-            Map<String, List<File>> sorted = finder.sortShouldEIParse();
-
-            // Parse all the files that need a json file.
-            invokeEliteInsight(sorted.get("toEI"));
-
-            // Add all the new json files
-            List<File> files = sorted.get("json");
-            FileManager findJson = new FileManager(sorted.get("toEI").toArray());
-            files.addAll(findJson.findEIParsedFiles());
-
+            List<File> files = LogCompare.processFiles(fileToDo.toArray(), log);
             for (File f : files) {
                 saveOutputJson(FilenameUtils.getBaseName(f.getName()), f);
             }
 
+            // close connection now that we're done
             try {
                 con.close();
             } catch (SQLException se) {
                 se.printStackTrace();
             }
-
         });
 
         return button;
+    }
+
+    private void reopenConnection() {
+        try {
+            if (con.isClosed()) {
+                con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/logcompare", "root", pass);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private void createSaveFolder() {
+        if (saveLocation.equals(DEFAULT_SAVE)) {
+            File theDir = new File(DEFAULT_SAVE);
+            if (!theDir.exists()){
+                theDir.mkdirs();
+            }
+        }
     }
 
     // EFFECT: save JSONObject to .json file to saveLocation
@@ -140,6 +133,7 @@ public class MainPanel extends JPanel {
         try {
             JSONObject output = app.compare(json);
             String name = evtc + "_LOGCOMPARE";
+
             JsonWriter writer = new JsonWriter(saveLocation, name);
             writer.open();
             writer.write(output);
@@ -148,46 +142,5 @@ public class MainPanel extends JPanel {
         } catch (Exception e) {
             log.append(e.getLocalizedMessage()+ "\n");
         }
-    }
-
-    // EFFECT: create a string array of commands for the EliteInsight parser
-    private String[] createCommandArray(List<File> evtcFiles) {
-        String[] result = new String[evtcFiles.size() + 3];
-        result[0] = "./data/GW2EI/GuildWars2EliteInsights.exe";
-        result[1] = "-c";
-        try {
-            result[2] = "\"" + new File("./data/assets/app.conf").getCanonicalPath() + "\"";
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < evtcFiles.size(); i++) {
-            File file = evtcFiles.get(i);
-            try {
-                result[3 + i] = "\"" + file.getCanonicalPath() + "\"";
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return result;
-    }
-
-    // EFFECT: run GuildWars2EliteInsights.exe
-    private void invokeEliteInsight(List<File> evtcFiles) {
-        try {
-            String[] params = createCommandArray(evtcFiles);
-
-            Process p = Runtime.getRuntime().exec(params);
-            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-            String line;
-            while ((line = input.readLine()) != null) {
-                log.append(line+ "\n");
-            }
-
-            input.close();
-        } catch (IOException e) {
-            log.append("Exception occurred: " + e+ "\n");
-        }
-
     }
 }
