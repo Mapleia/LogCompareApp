98960dd05c8acd64e5d17acbc949668582d8acdf
Mapleia <36678302+Mapleia@users.noreply.github.com>
2021-01-04 21:20:22 -0500

    updated parsed log paths

--- src/test/TestReader.java
+++ src/test/TestReader.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestReader {
     @Test
     public void testHashCode() {
-        JsonReader reader = new JsonReader("./data/Arkk/20201217-233716_arkk_kill.json",
-                "20201217-233716_arkk_kill");
-        JsonReader reader2 = new JsonReader("./data/Arkk/20201218-013716_arkk_kill.json",
-                "20201218-013716_arkk_kill");
+        JsonReader reader = new JsonReader("./data/parsed/20201228-234846_mama_kill.json",
+                "20201228-234846_mama_kill.json");
+        JsonReader reader2 = new JsonReader("./data/parsed/20201228-234846_mama_kill.json",
+                "20201228-234846_mama_kill.json");
 
         Input file1 = reader.read();
         Input file2 = reader2.read();
@@ -21,8 +21,8 @@ public class TestReader {
 
     @Test
     public void testRead() {
-        JsonReader reader = new JsonReader("./data/parsed/20201226-040423_sh_kill.json",
-                "20201226-040423_sh_kill.json");
+        JsonReader reader = new JsonReader("./data/parsed/20201228-234846_mama_kill.json",
+                "20201228-234846_mama_kill.json");
         Input file1 = reader.read();
         file1.hashCode();
     }
